ba74ee3d8d071bb179acb970f44dc61f5a36c58b
pedroramos17 <pedro_ramos2003@hotmail.com>
2023-08-15 21:08:05 -0300

    add object player

--- app/src/main/java/br/com/etecia/deixaelasjogarem/Player.java
+++ app/src/main/java/br/com/etecia/deixaelasjogarem/Player.java
@@ -1,4 +1,27 @@
 package br.com.etecia.deixaelasjogarem;
 
 public class Player {
+    private String title;
+    private int img;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int imgid) {
+        this.img = imgid;
+    }
+
+    public Player(int img) {
+        this.title = title;
+        this.img = img;
+    }
 }
---
+++ app/src/main/java/br/com/etecia/deixaelasjogarem/RecycleViewAdapter.java
@@ -0,0 +1,4 @@
+package br.com.etecia.deixaelasjogarem;
+
+public class RecycleViewAdapter {
+}
